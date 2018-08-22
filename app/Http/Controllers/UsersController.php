2da3ee491f6c0de5ab4adc81b229b2bc6ad5a7a5
crlccc <crl9606@outlook.com>
2018-08-22 16:57:00 +0800

    增加代码生成器

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
@@ -10,6 +11,11 @@ class UsersController extends Controller
 {
     //
 
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show',compact('user'));
@@ -17,12 +23,26 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
-    public  function update(UserRequest  $request,User $user)
+    public  function update(UserRequest  $request,ImageUploadHandler $uploader,User $user)
     {
-        $user->update($request->all());
-        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
+
+        $this->authorize('update',$user);
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id,362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
